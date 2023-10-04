36d5a8403c156eb92cdb525df8c747630b1bab4a
Maxeu444 <maxhochart.dev@gmail.com>
2023-10-04 16:46:43 +0200

    correction

--- JavaSemaine2/src/main/java/org/example/exception/Exercice3.java
+++ JavaSemaine2/src/main/java/org/example/exception/Exercice3.java
@@ -3,11 +3,11 @@ package org.example.exception;
 public class Exercice3 {
         public static void main(String[] args) {
 
-            int[] tableau = new int[5];
+            int[] tab = new int[5];
 
             try {
-                int sixiemeElement = tableau[5];
-                System.out.println("La valeur du sixième élément est : " + sixiemeElement);
+                int index6 = tab[5];
+                System.out.println("La valeur du sixième élément est : " + index6);
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("Erreur : Votre tableau est plus petit \n que l'élément auquel vous souhaitez accéder.");
             }
